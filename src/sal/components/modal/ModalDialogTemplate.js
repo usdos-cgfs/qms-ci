875e3ce50abf3dbcd569e64171230b313c17fa9f
Peter Backlund <backlundpf@state.gov>
2024-07-22 13:49:22 -0400

    modal dialog styles

--- src/sal/components/modal/ModalDialogTemplate.js
+++ src/sal/components/modal/ModalDialogTemplate.js
@@ -3,14 +3,14 @@ import { html } from "../../infrastructure/index.js";
 export const modalDialogTemplate = html`
   <dialog
     id=""
-    class="card bg-dark draggable modal-dialog"
+    class="draggable sal-modal-dialog"
     data-bind="attr: {id: getUniqueId() }"
   >
     <!-- Can't use 'with: currentDialog' since we need to register our 
       javascript event listeners for grabbing and resizing -->
-    <div class="card-header bg-dark grabber">
-      <h2 class="card-title" data-bind="text: title"></h2>
-      <h2 class="card-title">
+    <div class="modal-dialog-header grabber">
+      <h2 class="modal-dialog-title" data-bind="text: title"></h2>
+      <h2 class="modal-dialog-title">
         <i class="fa-solid fa-xmark pointer" data-bind="click: clickClose"></i>
       </h2>
     </div>
@@ -21,10 +21,10 @@ export const modalDialogTemplate = html`
       </ul>
     </div>
     <div
-      class="card-body"
+      class="modal-dialog-body"
       data-bind="component: { name: form.componentName, params: form.params }"
     ></div>
-    <div class="card-actions">
+    <div class="modal-dialog-actions">
       <button
         style
         type="button"
@@ -37,7 +37,7 @@ export const modalDialogTemplate = html`
   </dialog>
 
   <style>
-    .modal-dialog {
+    .sal-modal-dialog {
       display: none;
       position: absolute;
       z-index: 15;
@@ -48,10 +48,43 @@ export const modalDialogTemplate = html`
       margin: 0;
       top: 125px;
       resize: both;
+
+      border: 2px solid var(--primary-color);
+      border-radius: 6px;
+      display: flex;
+      flex-direction: column;
     }
 
-    .modal-dialog.active {
+    .sal-modal-dialog.active {
       display: flex;
+    }
+
+    .sal-modal-dialog .modal-dialog-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      /* background-color: inherit; */
+      /* position: sticky;
+      top: 0; */
+      padding: 1.5rem 1.5rem 0.5rem 1.5rem;
+    }
+
+    .sal-modal-dialog .modal-dialog-title {
+      color: inherit;
+      margin: 0;
+    }
+
+    .sal-modal-dialog .modal-dialog-body {
+      padding: 0.5rem 1.5rem;
+      overflow: auto;
+    }
+
+    .sal-modal-dialog .modal-dialog-actions {
+      display: flex;
+      justify-content: flex-end;
+      gap: 1rem;
+      margin-top: auto;
+      padding: 0.5rem 1.5rem 1.5rem 1.5rem;
     }
 
     .draggable {
